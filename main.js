f05d0d50fd1eddf604386ea1085f40e252ae4526
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2024-03-26 08:49:09 -0300

    .1

--- main.js
+++ main.js
@@ -22,5 +22,23 @@ const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date ();
     let tempoFinal = tempoObjetivo-tempoAtual;
+    let segundos = Math.floor (tempoFinal/1000);
+    let minutos = Math.floor (minutos/60);
+    let horas = Math.floor (minutos/60);
+    let dias = Math.floor (horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+        } else {
+            return "Prazo Finalizado";
+        }
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
+
+
 
