72c80c76b5b8812e2ef0d42cae694f6308997ff8
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2024-03-26 08:35:05 -0300

    .1

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
@@ -9,3 +10,12 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2024-04-21T13:00:00");
+const tempoObjetivo2 = new Date ("2024-04-30T15:00:00");
+const tempoObjetivo3 = new Date ("2024-08-10T12:00:00");
+const tempoObjetivo4 = new Date ("2025-02-01T07:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
